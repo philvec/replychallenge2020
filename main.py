b14c007bd726079d1593c70a2ed0818e6f6c2489
Michal Karbownik <michakarbownik@gmail.com>
2020-03-12 18:48:10 +0100

    Add Michal's main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+### MAIN MICHAL ###
+from loader import * 
 filepaths = ['data/a_solar.txt', 'data/b_dream.txt', 'data/c_soup.txt', 'data/d_maelstrom.txt', 'data/e_igloos.txt', 'data/f_glitch.txt']
 
-if __name__ == '__main__':
-    print(0)
+loader = Loader()
+loader.load(filepaths[0])
