0f25ab9d4ccfec3263b985a4a04a58aa08ee8203
snufkin <strzalkafilip@gmail.com>
2020-03-12 17:46:13 +0100

    fitness.py methods initialised

---
+++ fitness.py
@@ -0,0 +1,16 @@
+
+def best_friend_for_worker(worker, friends):
+    # result = [(friend, fitness), ...]
+    return []
+
+def best_worker_for_seat(seat, workers):
+    # result = [(worker, fitness), ...]
+    return []
+
+
+
+def fitness_w_w(worker1, worker2):
+    return 1
+
+def fitness_s_w(seat, worker):
+    return 1
