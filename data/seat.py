f292ce76bb0684ea9d558f474e20adc659954005
snufkin <strzalkafilip@gmail.com>
2020-03-12 17:12:44 +0100

    Seat model implemented

--- data/seat.py
+++ data/seat.py
@@ -1,8 +1,8 @@
 
 class Seat:
-    def __init__(self, param1, param2):
-        #konstruktor
-        self.param1 = param1
-
-    def metoda1(self):
-        pass
+    def __init__(self, x, y, type=0, adjacent_seats=[0, 0, 0, 0]):
+        self.x = x
+        self.y = y
+        self.type = type         # <--  {0-no, 1-DEVseat, 2-PMseat}
+        # [left, up, right, down]  <--  {0-no, 1-DEVseat, 2-PMseat}
+        self.adjacent_seats = adjacent_seats
