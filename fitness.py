b839fb71e7341dabca94f527939c11e5e868702b
snufkin <strzalkafilip@gmail.com>
2020-03-12 17:57:06 +0100

    fitness.py methods initialised

--- fitness.py
+++ fitness.py
@@ -14,8 +14,8 @@ def fitness_w_w(worker1, worker2):
     all_skills = [el for el in worker1.skills if el not in worker2.skills] + worker2.skills
     wp = len(common_skills) * len(all_skills)
 
-    if worker1.company == worker2.company:
-        bp = 0
+    bp = worker1.bonus * worker2.bonus if worker1.company == worker2.company else 0
+
     return wp + bp
 
 def fitness_s_w(seat, worker):
