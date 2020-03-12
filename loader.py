03328ff22c7719271c26ecd6d4c4102505ef0615
Michal Karbownik <michakarbownik@gmail.com>
2020-03-12 17:55:32 +0100

    Move parsing to a separate file

--- loader.py
+++ loader.py
@@ -1,3 +1,5 @@
+from data.seat import *
+from data.worker import *
 
 class Loader():
     def __init__(self):
@@ -7,4 +9,53 @@ class Loader():
         # seats = []   <- list of Seat
         # workers = []  <- list of Worker
         # return (seats, workers)
+        seats = []
+        developers = []
+        projectManagers = []
+
+        with open(filepaths[0]) as fp:
+            ### MAP PARSING ###
+            firstLine = fp.readline()
+            width, height = firstLine.split()
+            width = int(width)
+            height = int(height)
+
+            for h in range(height):
+                mapLine = list(fp.readline())
+                
+                for w in range(width):
+                    seats.append(Seat(w, h, mapLine[w]))
+            
+            ### DEVELOPER PARSING ###
+            line = fp.readline()
+            numberOfDevs = int(line)
+
+            for dev in range(numberOfDevs):
+                line = fp.readline()
+                developer = line.split()
+                
+                company = developer[0]
+                potentialBonus = int(developer[1])
+                numberOfSkills = int(developer[2])
+                skills = []
+
+                for i in range(numberOfSkills):
+                    skills.append(developer[2 + i])
+
+                developers.append(Worker('D', potentialBonus, skills))
+
+            ### PM PARSING ###
+            line = fp.readline()
+            numberOfPMs = int(line)
+
+            for PM in range(numberOfPMs):
+                line = fp.readline()
+                PM = line.split()
+                
+                company = PM[0]
+                potentialBonus = int(PM[1])
+
+                projectManagers.append(Worker('P', potentialBonus))
+
+        return (seats, developers + projectManagers)
         pass
