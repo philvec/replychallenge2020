fe30da18101ce162fc3d0b85b1267fca9dc90ddf
snufkin <strzalkafilip@gmail.com>
2020-03-12 17:56:08 +0100

    fitness.py methods initialised

--- data/worker.py
+++ data/worker.py
@@ -1,6 +1,7 @@
 
 class Worker:
-    def __init__(self, role, bonus=0, skills=[]):
+    def __init__(self, company, role, bonus=0, skills=[]):
+        self.company = company
         self.role = role        # <--  {'D', 'P'}
         self.bonus = bonus
         self.skills = skills
--- fitness.py
+++ fitness.py
@@ -10,8 +10,12 @@ def best_worker_for_seat(seat, workers):
 
 
 def fitness_w_w(worker1, worker2):
-    wp = 0
-    bp = 0
+    common_skills = [el for el in worker1.skills if el in worker2.skills]
+    all_skills = [el for el in worker1.skills if el not in worker2.skills] + worker2.skills
+    wp = len(common_skills) * len(all_skills)
+
+    if worker1.company == worker2.company:
+        bp = 0
     return wp + bp
 
 def fitness_s_w(seat, worker):
